9665708ac4a8b1520d85410e0d239028b2cb30f6
peanut996 <peanut996@qq.com>
2021-03-08 01:44:45 +0800

    Add react project

---
+++ peanut996.im.react/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          peanut996.im 
+        </p>
+        <p>
+          powered by   
+        <a> </a>
+        <a
+          className="App-link"
+          href="https://github.com/peanut996/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           peanut996
+        </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
