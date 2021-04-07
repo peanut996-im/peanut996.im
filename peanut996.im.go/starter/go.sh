b4263d042e64dc7452d83bd2817cb50b4c119738
peanuts <849421294godw@gmail.com>
2021-04-07 18:35:35 +0800

    Fix go.sh

--- peanut996.im.go/starter/go.sh
+++ peanut996.im.go/starter/go.sh
@@ -5,6 +5,9 @@
 
 appName=${PWD##*/}
 targetos=`uname | tr "[A-Z]" "[a-z]"`
+if [[ $targetos == "mingw"* ]];then
+targetos="windows"
+fi
 bash ./build.sh $targetos
 
 echo "./bin/$appName -f ./etc/config.yaml $1 $2 $3 $4"
